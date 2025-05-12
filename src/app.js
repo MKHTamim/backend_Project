24b518e821816852e02373e011fcb539f9a8f17f
mkht <mkhtcode@gmail.com>
2025-05-12 12:06:03 +0600

    feat:user router added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
+
+app.use("/",(req,res)=>{
+    res.send("<h1>this is home route form /</h1>")
+})
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true,
@@ -12,5 +17,15 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
 .catch((err) => {
     console.error("MongoDB Connection Error !! ", err)
