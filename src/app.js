2239e5141abd421cd7300e1de2dd8963c7997435
mkht <mkhtcode@gmail.com>
2025-05-13 11:01:01 +0600

    fix: get the response using postman and thunderClient done !

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 //routes import
 
-import userRouter from './routes/user.routes'
+import userRouter from './routes/user.routes.js'
 
 //routes declaration
 app.use("/api/v1/users" , userRouter)
